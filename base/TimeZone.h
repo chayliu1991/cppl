14b3e2324940741f55842ee65256a1fa08b87444
chayliu <chayliu@126.com>
2022-04-06 11:03:17 +0800

    update base

--- base/TimeZone.h
+++ base/TimeZone.h
@@ -1,36 +1,36 @@
-// #pragma once
+#pragma once
 
-// #include <memory>
-// #include <time.h>
+#include <memory>
+#include <time.h>
 
-// #include "Copyable.h"
+#include "Copyable.h"
 
-// namespace cppl
-// {
-//     class TimeZone : public Copyable
-//     {
-//     public:
-//         struct Data;
+namespace cppl
+{
+    class TimeZone : public Copyable
+    {
+    public:
+        struct Data;
 
-//         explicit TimeZone(const char *zonefile);
-//         TimeZone(int eastOfUtc, const char *tzname);
-//         TimeZone() = default;
+        explicit TimeZone(const char *zonefile);
+        TimeZone(int eastOfUtc, const char *tzname);
+        TimeZone() = default;
 
-//         bool valid() const
-//         {
-//             return static_cast<bool>(data_);
-//         }
+        bool valid() const
+        {
+            return static_cast<bool>(data_);
+        }
 
-//         struct tm toLocalTime(time_t secondsSinceEpoch) const;
-//         time_t fromLocalTime(const struct tm &) const;
+        struct tm toLocalTime(time_t secondsSinceEpoch) const;
+        time_t fromLocalTime(const struct tm &) const;
 
-//         static struct tm toUtcTime(time_t secondsSinceEpoch, bool yday = false);
+        static struct tm toUtcTime(time_t secondsSinceEpoch, bool yday = false);
 
-//         static time_t fromUtcTime(const struct tm &);
+        static time_t fromUtcTime(const struct tm &);
 
-//         static time_t fromUtcTime(int year, int month, int day, int hour, int minute, int seconds);
+        static time_t fromUtcTime(int year, int month, int day, int hour, int minute, int seconds);
 
-//     private:
-//         std::shared_ptr<Data> data_;
-//     };
-// }
+    private:
+        std::shared_ptr<Data> data_;
+    };
+}
