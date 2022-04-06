c6d7df7ec849b40686bfd9e855187b9bf20397bb
chayliu <chayliu@126.com>
2022-04-06 13:21:21 +0800

    update base

---
+++ base/tests/LogFile_test.cpp
@@ -0,0 +1,36 @@
+#include "../LogFile.h"
+#include "../Logging.h"
+
+#include <unistd.h>
+
+std::unique_ptr<cppl::LogFile> g_logFile;
+
+void outputFunc(const char *msg, int len)
+{
+    g_logFile->append(msg, len);
+}
+
+void flushFunc()
+{
+    g_logFile->flush();
+}
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+
+    char name[256] = {'\0'};
+    strncpy(name, argv[0], sizeof name - 1);
+    g_logFile.reset(new cppl::LogFile(::basename(name), 200 * 1000));
+    cppl::Logger::setOutput(outputFunc);
+    cppl::Logger::setFlush(flushFunc);
+
+    cppl::string line = "1234567890 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+    for (int i = 0; i < 10000; ++i)
+    {
+        LOG_INFO << line << i;
+
+        usleep(1000);
+    }
+}
